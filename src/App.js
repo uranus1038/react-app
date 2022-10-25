a790064c0f193bb5b6b7c372ce9c1f7c1935b454
Anusorn <anusorn1039@gmail.com>
2022-10-25 19:21:36 +0700

    Add First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+//Components
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+//css
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        < nav class="navbar navbar-expand-lg navbar-dark UMI_BG_SKYBLUE" >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Uranus x Star</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Update</a>
+                        </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Game
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                            <li><a class="dropdown-item" href="#">Generator of Aumi</a></li>
+
+                        </ul>
+                    </li>
+                    </ul>
+
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav >
+    )
+}
+
+export default Navbar; 
